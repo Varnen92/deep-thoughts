c11370e1dc4a7006780bdf876fcba682b207f383
Robert Sandoval <robsand1992@yahoo.com>
2022-02-23 10:34:14 -0800

    updated and built out profile/thoughts pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { useQuery } from '@apollo/client'
-import { QUERY_THOUGHTS } from '../utils/queries'
-import ThoughtList from '../components/ThoughtList'
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
-  const { loading, data} = useQuery(QUERY_THOUGHTS)
-  const thoughts = data?.thought || []
-  console.log(thoughts)
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList thoughts={thoughts} title="Some feed for Thought(s)..." />
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
           )}
         </div>
       </div>
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express')
-const { authMiddleWare} = require('./utils/auth')
-const { typeDefs, resolvers } = require('./schemas')
+const {ApolloServer} = require('apollo-server-express');
+const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const path = require('path')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,28 +13,26 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleWare
-  })
-
-  await server.start()
-
-  server.applyMiddleware({ app })
-
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-}
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
 startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
