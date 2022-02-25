e8c339da750feecfa43e91090d681102bf08f867
Robert Sandoval <robsand1992@yahoo.com>
2022-02-25 10:38:41 -0800

    updated queries to include auth

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,17 +3,19 @@ import { useParams } from 'react-router-dom';
 import FriendList from '../components/FriendList';
 import ThoughtList from '../components/ThoughtList';
 
+
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
 
   if (loading) {
     return <div>Loading...</div>;
